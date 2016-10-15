3330aec435baa86872762841f56e77f450466f08
Sean McGowan <spmcgowa@mtu.edu>
2016-10-15 19:57:31 -0400

    Changed how interacting with the computer changes the camera. Clicking on the computer zooms in, clicking off of the computer zooms out.

---
+++ Assets/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraChange : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+	
+	public Camera cam1;
+	public Camera cam2;
+	public GameObject listener;
+	public GameObject mainCamera;
+
+
+	void Start () {
+		cam1.enabled = true;
+		cam2.enabled = false;
+	}
+	void Update(){
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)) {
+			if (hit.collider.gameObject == listener) {
+				if (Input.GetMouseButtonDown (0)) {
+					cam1.enabled = false;
+					cam2.enabled = true;
+				}
+			}else if (hit.collider.gameObject != listener) {
+				if (Input.GetMouseButtonDown (0)) {
+					cam1.enabled = true;
+					cam2.enabled = false;
+					Screen.lockCursor = true;
+					mainCamera.GetComponent<UnityStandardAssets.Utility.SimpleMouseRotator> ().centerCam ();
+					Screen.lockCursor = false;
+				}
+			}
+		}
+	}
+}
