a48eac2d0b74711f3113f56e866f1d54171561a4
Evan Ronnei <etronnei@mtu.edu>
2016-10-26 17:45:14 -0400

    Added sounds and music changing

---
+++ Assets/Scripts/MusicChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicChange : MonoBehaviour {
+	
+	public AudioSource song1;
+	
+	void Start() {
+		song1.enabled = false;
+	}
+	
+	void OnMouseDown() {
+		song1.enabled = !song1.enabled;
+	}
+}
