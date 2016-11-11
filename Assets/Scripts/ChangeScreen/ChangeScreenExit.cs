1294ba5af7f990eba3fd6321add50d75b2383915
Evan Ronnei <etronnei@mtu.edu>
2016-11-11 18:32:11 -0500

    added screen changing

---
+++ Assets/Scripts/ChangeScreen/ChangeScreenExit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScreenExit : MonoBehaviour {
+	
+	public GameObject desktop;
+	public GameObject map;
+	public GameObject info;
+	public GameObject terminal;
+	public GameObject mapSelect;
+	public GameObject infoSelect;
+	public GameObject terminalSelect;
+	public GameObject exit;
+	
+	void OnMouseDown() {
+		desktop.SetActive(true);
+		map.SetActive(false);
+		info.SetActive(false);
+		terminal.SetActive(false);
+		mapSelect.SetActive(true);
+		infoSelect.SetActive(true);
+		terminalSelect.SetActive(true);
+		exit.SetActive(false);
+	}
+}
