3006179d76437ffbcfde31fa5934ee1b4187db14
Max Faust-Smith <msfausts@mtu.edu>
2016-11-30 17:23:47 -0500

    Revert "Some things wooo"
    This reverts commit 31c0146d45c7a4a400a1f42401878b037d305f27.

---
+++ Assets/Scripts/CallGeneration/Old/Dialog.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class Dialog {
+
+	private string _tag;
+	private string _text;
+	private List<Response> _responses;
+
+
+
+	// Use getters and setters to disable setting!
+	public string tag { get {return _tag; } set {}}
+	public string text  { get {return _text; } set {}}
+	public List<Response> responses  { get {return _responses; } set {}}
+
+
+	//Constructors
+	public Dialog(string tag, string text, List<Response> responses) {
+		this._tag = tag;
+		this._text = text;
+		this._responses = responses;
+	}
+
+
+	[System.Serializable]
+	public class Response {
+		private string _text;
+		private string _link;
+
+		//Cannot set after created
+		public string text { get {return _text;} set { } }
+		public string link { get {return _link;} set { } }
+
+		//Constructor
+		public Response(string text, string link) {
+			this._text = text;
+			this._link = link;
+		}
+	}
+}
---
+++ Assets/Scripts/CallGeneration/Old/RandomCallGenerator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class RandomCallGenerator : MonoBehaviour {
+
+	public Call GenerateCall() {
+		string callerName = GenerateName();
+		float callLegitimacy = GenerateLegitimacy();
+		Location callLocation = GenerateLocation();
+		Call.CallType callType = GenerateCalltype(callLocation);
+		int scale = GenerateScale();
+
+		// set up those values
+
+		Call newCall = new Call();
+		newCall.Setup(callerName,callLegitimacy,callLocation,callType, scale, new List<Dialog>());
+		newCall.DebugPrint();
+		return newCall;
+	}
+
+	private string GenerateName() {
+		return "John Doe";
+	}
+	private float GenerateLegitimacy() {
+		if (Random.value <= .2f)
+			return 0.0f;
+		else 
+			return 1.0f;
+	}
+	private Location GenerateLocation() {
+		//Grab a random location and return it
+		int locNum = (int)Mathf.Floor(MapInformation.gameMapInfo.Locations.Length * Random.value);
+		return MapInformation.gameMapInfo.Locations[locNum];
+	}
+	private Call.CallType GenerateCalltype(Location local) {
+		// Calculate a call based on the Animation curves as probibilites.
+		float total = local.crimeStats + local.fireStats + local.medicalStats;
+		float rng = total * Random.value;
+		if (rng <= local.crimeStats) 
+			return Call.CallType.Crime;
+		else if (rng <= local.crimeStats+local.fireStats)
+			return Call.CallType.Fire;
+		else 
+			return Call.CallType.Medical;
+	}
+
+	// 5 levels of scale, 1 relativly minor/common, 5 the worst possible
+	private int GenerateScale() {
+		// .7, .15, .07, .05, .03
+		float rng = Random.value;
+		if (rng <= .7) return 1;
+		else if (rng <= .85) return 2;
+		else if (rng <= .92) return 3;
+		else if (rng <= .97) return 4;
+		else return 5;
+	}
+}
