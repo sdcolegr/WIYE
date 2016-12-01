83ce7f060ef98da13caab94dfa0adeacbc603a15
JUSTINSLAPTOP\Justin <jwcain@mtu.edu>
2016-11-30 19:51:12 -0500

    Added menus to the map to be able to handle just one call for the demo.

---
+++ Assets/MapButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapButton : MonoBehaviour {
+
+	public GameObject popupMenu;
+	public void OnMouseDown() {
+		popupMenu.transform.position = this.transform.position;
+	}
+}
